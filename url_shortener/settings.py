ae4840dfc019917eec507d7822e3e1fe57c9a630
Sarthak5598 <sarthak5598sharma@gmail.com>
2024-08-19 02:56:39 +0530

    added allowed hosts

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = "django-insecure-z4+qr*fve%@#dl_o&dpxzmf&8%idxw!)4e0(3%^@1$ae)*g3=k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['url-shortener-dwke.onrender.com', '127.0.0.1', 'localhost']
+
 
 
 # Application definition
