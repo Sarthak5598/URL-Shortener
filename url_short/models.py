25c16ef564d9e0e541cb8f521baa504c9122fae6
Sarthak5598 <sarthak5598sharma@gmail.com>
2024-08-18 22:07:19 +0530

    Made a model to store urls

--- url_short/models.py
+++ url_short/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class URL (models.Model):
+    original_url=models.URLField(max_length=1000)
+    short_url=models.CharField(max_length=100)
+    created=models.DateField()
+    
+    def __str__(self) -> str:
+        return f"{self.original_url} changed to {self.short_url}"
