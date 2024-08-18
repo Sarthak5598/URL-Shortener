fd09e63aff71a908c74020e6f7a3b13ac9f8b86b
Sarthak5598 <sarthak5598sharma@gmail.com>
2024-08-18 21:56:17 +0530

    Added other nessecary structure

--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path,include
 urlpatterns = [
+    path("",include('url_short.urls')),
     path("admin/", admin.site.urls),
 ]
