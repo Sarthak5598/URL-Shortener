961d5209571722e6e81a2cc02bb57378720126c4
Sarthak5598 <sarthak5598sharma@gmail.com>
2024-09-22 01:14:17 +0530

    added login feature also added daily limit for users

--- url_short/models.py
+++ url_short/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 class URL (models.Model):
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
     original_url=models.URLField(max_length=1000)
     short_code=models.CharField(max_length=100)
     created=models.DateTimeField()
-    
+     
     def __str__(self) -> str:
         return f"{self.original_url} changed to {self.short_code}"
+    
+class UserUsage(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    last_reset = models.DateField(default=timezone.now().date())  # Store only the date
+    usage_count = models.IntegerField(default=0)
--- url_short/urls.py
+++ url_short/urls.py
@@ -1,9 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import home,redirect_to_original
+from .views import home,redirect_to_original,registration_page,login_page,logoutUser
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home,name='home'),
+    path("register/",registration_page,name="registration"),
+    path("login/",login_page,name="login"),
+    path("logout/",logoutUser,name="logout"),
     path('<short_code>/', redirect_to_original, name='redirect'),
 ]
